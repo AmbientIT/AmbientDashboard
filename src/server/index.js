b78ce5022ca4dcef329f3a4c31f6811b09a721a9
latest <charles.jacquin@autistici.org>
2016-09-05 13:09:35 +0200

    switch to koa-wbpack-middleware

--- src/server/index.js
+++ src/server/index.js
@@ -10,7 +10,6 @@ import createApis from './api'
 import { logger, getConfiguredContainer, env } from './_core'
 import {
   notFoundHandler,
-  waitForWebpack,
   webpackDevMiddleware,
   serveFrontMiddleware,
 } from './middlewares'
@@ -30,27 +29,26 @@ export default async () => {
   if (env.DEV) {
     app.use(koaLogger())
     app.use(webpackDevMiddleware())
-    await waitForWebpack()
   }
   app.use(serveFrontMiddleware())
 
-  // Container is configured with our services and whatnot.
-  // const container = getConfiguredContainer()
+// Container is configured with our services and whatnot.
+  const container = getConfiguredContainer()
 
-  // Creates an Awilix scope per request. Check out the awilix-koa
-  // docs for details: https://github.com/jeffijoe/awilix-koa
-  // app.use(scopePerRequest(container))
-  //
-  // // Adds middleware that creates a new Container Scope for each request.
-  // app.use((ctx, next) => {
-  //   // faking authentication just to demo Awilix capabilities
-  //   ctx.state.container.registerValue({
-  //     currentUser: {
-  //       id: ctx.request.query.userId,
-  //     },
-  //   })
-  //   return next()
-  // })
+// Creates an Awilix scope per request. Check out the awilix-koa
+// docs for details: https://github.com/jeffijoe/awilix-koa
+  app.use(scopePerRequest(container))
+
+// Adds middleware that creates a new Container Scope for each request.
+  app.use((ctx, next) => {
+    // faking authentication just to demo Awilix capabilities
+    ctx.state.container.registerValue({
+      currentUser: {
+        id: ctx.request.query.userId,
+      },
+    })
+    return next()
+  })
 
   const router = new Router()
   try {
--- src/server/middlewares/webpack.js
+++ src/server/middlewares/webpack.js
@@ -1,28 +1,17 @@
 import webpack from 'webpack'
-import connect from 'koa-connect'
 import convert from 'koa-convert'
 import compose from 'koa-compose'
-import webpackDev from 'webpack-dev-middleware'
+import webpackDev from 'koa-webpack-dev-middleware'
 import webpackHot from 'koa-webpack-hot-middleware'
 import webpackConfig from '../../client/_webpack/common'
 
 const compiler = webpack(webpackConfig)
 
-const webpackDevInstance = webpackDev(compiler, {
-  noInfo: true,
-  publicPath: webpackConfig.output.publicPath,
-})
-
-/**
- * wait for webpack to finish his job
- * @return {Promise<void>} resolve when ready
- */
-export const waitForWebpack = () => {
-  return new Promise(resolve => webpackDevInstance.waitUntilValid(resolve))
-}
-
 export const webpackDevMiddleware = () => compose([
-  convert(connect(webpackDevInstance)),
+  convert(webpackDev(compiler, {
+    noInfo: true,
+    publicPath: webpackConfig.output.publicPath,
+  })),
   convert(webpackHot(compiler, {
     log: console.log,
     path: '/__webpack_hmr',
