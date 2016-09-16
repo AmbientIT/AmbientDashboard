36e9961f8556c74451d68d17af08aaa63d91f24c
latest <charles.jacquin@autistici.org>
2016-09-16 03:56:51 +0200

    refactor(fetch): replace request-promise by isomorphic-fetch on the server

--- src/server/api/auth/auth.router.js
+++ src/server/api/auth/auth.router.js
@@ -1,3 +1,4 @@
+import createError from 'http-errors'
 import User from '../../models/mongoose/user'
 import { createJWT, getGoogleToken, getGoogleProfile } from '../../services/auth'
 import isAuthenticated from '../../middlewares/guards/isAuthenticated'
@@ -6,33 +7,38 @@ export default router => {
   router.get('/auth/me', isAuthenticated, ctx => ctx.ok(ctx.user))
 
   router.post('/auth/google', async ctx => {
-    const { access_token } = await getGoogleToken({
-      code: ctx.request.body.code,
-      'client_id': ctx.request.body.clientId, //eslint-disable-line
-      'redirect_uri': ctx.request.body.redirectUri, //eslint-disable-line
-    })
-
-    const profile = await getGoogleProfile(access_token)
-
-    if (profile.error) {
-      throw profile.error
-    }
-
-    const userCount = await User.count({})
-    let user = await User.findOne({ google: profile.sub })
-    if (!user) {
-      const newUser = new User({
-        google: profile.sub,
-        avatar: profile.picture.replace('sz=50', 'sz=200'),
-        name: profile.name,
-        email: profile.email,
-        role: userCount > 0 ? 'peon' : 'admin',
+    try {
+      const { access_token } = await getGoogleToken({
+        code: ctx.request.body.code,
+        'client_id': ctx.request.body.clientId, //eslint-disable-line
+        'redirect_uri': ctx.request.body.redirectUri, //eslint-disable-line
       })
-      user = await newUser.save()
+
+      const profile = await getGoogleProfile(access_token)
+
+      if (profile.error) {
+        throw createError(401, profile.error.message)
+      }
+
+      const userCount = await User.count({})
+      let user = await User.findOne({ google: profile.sub })
+      if (!user) {
+        const newUser = new User({
+          google: profile.sub,
+          avatar: profile.picture.replace('sz=50', 'sz=200'),
+          name: profile.name,
+          email: profile.email,
+          role: userCount > 0 ? 'peon' : 'admin',
+        })
+        user = await newUser.save()
+      }
+      ctx.ok({
+        token: createJWT(user),
+        user,
+      })
+    } catch (err) {
+      console.error(err)
+      throw createError(401, err.message)
     }
-    ctx.ok({
-      token: createJWT(user),
-      user,
-    })
   })
 }
--- src/server/services/auth.js
+++ src/server/services/auth.js
@@ -1,6 +1,9 @@
+/* eslint guard-for-in:0 */
+/* eslint no-restricted-syntax:0 */
 import jwt from 'jwt-simple'
 import moment from 'moment'
-import request from 'request-promise'
+import fetch from 'isomorphic-fetch'
+import FormData from 'isomorphic-form-data'
 import { env } from '../_core'
 
 export const createJWT = (user) => {
@@ -12,19 +15,25 @@ export const createJWT = (user) => {
 }
 
 export const getGoogleToken = (params) => {
-  return request.post('https://accounts.google.com/o/oauth2/token', {
-    json: true,
-    form: Object.assign(params, {
-      grant_type: 'authorization_code',
-      client_secret: env.google.GOOGLESECRET,
-    }),
+  const formData = new FormData()
+  const fetchBody = Object.assign(params, {
+    grant_type: 'authorization_code',
+    client_secret: env.google.GOOGLESECRET,
   })
+
+  for (const key in fetchBody) {
+    formData.append(key, fetchBody[key])
+  }
+
+  return fetch('https://accounts.google.com/o/oauth2/token', {
+    method: 'post',
+    body: formData,
+  }).then(response => response.json())
 }
 
 export const getGoogleProfile = (accessToken) => {
-  return request.get({
-    url: 'https://www.googleapis.com/plus/v1/people/me/openIdConnect',
+  return fetch('https://www.googleapis.com/plus/v1/people/me/openIdConnect', {
+    method: 'GET',
     headers: { Authorization: `Bearer ${accessToken}` },
-    json: true,
-  })
+  }).then(response => response.json())
 }
