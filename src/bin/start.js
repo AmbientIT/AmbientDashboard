a1c2d853fc7597b5785cf26c9d75d8cc9bc143ea
latest <charles.jacquin@autistici.org>
2016-10-18 14:41:40 +0200

    refactor(start): encapsulate the start script in a async IIFE

--- src/bin/start.js
+++ src/bin/start.js
@@ -2,13 +2,13 @@ import IntlPolyfill from 'intl'
 import createServer from '../server'
 import { env, logger } from '../server/_core'
 
-Intl.NumberFormat = IntlPolyfill.NumberFormat
-Intl.DateTimeFormat = IntlPolyfill.DateTimeFormat
-
-const PORT = env.PORT || 3000
-
-createServer().then(async app => {
+(async () => {
   try {
+    Intl.NumberFormat = IntlPolyfill.NumberFormat
+    Intl.DateTimeFormat = IntlPolyfill.DateTimeFormat
+
+    const PORT = env.PORT || 3000
+    const app = await createServer()
     await app.listen(PORT)
     const mode = env.NODE_ENV
     logger.debug(`Server listening on ${PORT} in ${mode} mode`)
@@ -16,4 +16,4 @@ createServer().then(async app => {
     logger.error('Error while starting up server', err)
     process.exit(1)
   }
-})
+})()
